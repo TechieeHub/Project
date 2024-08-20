0d283289b769f820305e6589e327583b36363635
TechieeHub <ishangupta141997@gmail.com>
2024-08-20 12:55:57 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Home from './Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ChartData from './components/Charts/ChartData'
 const App = () => {
   return (
     <>  
-    <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+        <Route path="charts" element={<ChartData />} />
+
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
