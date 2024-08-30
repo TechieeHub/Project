a8c0157c2d8ba47832ce349d14c7ec54342fcfe4
ShivangiInc <shivangimittal.0.4.0.1.9.6@gmail.com>
2024-08-30 15:55:15 +0530

    code for large run

--- src/components/Charts/ChartData.js
+++ src/components/Charts/ChartData.js
@@ -1,44 +1,50 @@
-import { Box } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import VisualizationComponent from './VisualizationComponent'
-import AnomalieComponent from './AnomalieComponent'
-import ChartComponent from './ChartComponent'
+import { Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import VisualizationComponent from './VisualizationComponent';
+import AnomalieComponent from './AnomalieComponent';
+import ChartComponent from './ChartComponent';
 import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios'
-import { setTableData } from '../../Store/excelSlice'
+import axios from 'axios';
+import { setTableData } from '../../Store/excelSlice';
 
 const ChartData = () => {
-  const [initialChartData,setInitialChartData]=useState(null)
+  const [filteredChartData, setFilteredChartData] = useState(null);
+  const [initialChartData, setInitialChartData] = useState(null);
 
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
   const excelData = useSelector((state) => state.excel.filteredData)?.filter(
     (data) => data.is_deleted !== true
   );
+
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/data/")
       .then((response) => {
         dispatch(setTableData(response.data.records));
-        setInitialChartData(response.data.records)
+        setInitialChartData(response.data.records);
       })
       .catch((error) => console.log("error", error));
   }, []);
 
+  const handleView = (data) => {
+    setFilteredChartData(data);
+  };
 
   return (
     <Box>
-    <Box sx={{display:'flex', gap:'10px', marginTop:'10px', marginLeft:'5px', marginRight:'5px'}}>
-      <VisualizationComponent/>
-      <AnomalieComponent/>
+      <Box sx={{ display: 'flex', gap: '10px', marginTop: '10px', marginLeft: '5px', marginRight: '5px' }}>
+        <VisualizationComponent onView={handleView} />
+        <AnomalieComponent />
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', marginTop: '30px' }}>
+        {filteredChartData?.length > 0 ? (
+          <ChartComponent data={filteredChartData} />
+        ) : (
+          initialChartData?.length > 0 && <ChartComponent data={initialChartData} />
+        )}
+      </Box>
     </Box>
-    <Box sx={{display:'flex', flexDirection:'column', alignItems:'center', gap:'10px', marginTop:'30px'}}>
-      {initialChartData?.length>0 && 
-      <ChartComponent data={excelData}/>      
-}
+  );
+};
 
-    </Box>
-    </Box>
-  )
-}
-
-export default ChartData
+export default ChartData;
--- src/components/Charts/VisualizationComponent.js
+++ src/components/Charts/VisualizationComponent.js
@@ -15,61 +15,50 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { setChartDisplayData } from "../../Store/excelSlice";
 
-const VisualizationComponent = () => {
-
-  const dispatch=useDispatch()
+const VisualizationComponent = ({ onView }) => {
+  const dispatch = useDispatch();
   const excelData = useSelector((state) => state.excel.data)?.filter(
     (data) => data.is_deleted !== true
   );
 
+  const largeRunViewHandler = () => {
+    const data = excelData.filter((data) => data.AccountRefresh === "Large run");
+    dispatch(setChartDisplayData(data));
+    onView(data); // Pass filtered data to parent component
+  };
 
-  const largeRunViewHandler=()=>{
-    const data=excelData.filter(data=>data.AccountRefresh==='Large run')
-    dispatch(setChartDisplayData(data))
-  }
+  const quickRunViewHandler = () => {
+    const data = excelData.filter((data) => data.AccountRefresh === "Quick run");
+    dispatch(setChartDisplayData(data));
+    onView(data); // Pass filtered data to parent component
+  };
 
-  const quickRunViewHandler=()=>{
-    const data=excelData.filter(data=>data.AccountRefresh==='Quick run')
-    dispatch(setChartDisplayData(data))
-  }
   return (
     <TableContainer component={Paper}>
-      <Typography
-        sx={{ fontSize: "25px", fontWeight: 600, backgroundColor: "grey" }}
-      >
+      <Typography sx={{ fontSize: "25px", fontWeight: 600, backgroundColor: "grey" }}>
         <Box sx={{ marginLeft: "15px" }}>Account Monitored</Box>
       </Typography>
       <Table>
         <TableHead>
-          
-        </TableHead>
-        <TableHead>
-          {/* <TableRow>
-            <TableCell sx={{ fontSize: "17px", fontWeight: 550 }}>
-              Total MDM IDs
-            </TableCell>
-            <TableCell>{excelData?.length}</TableCell>
-          </TableRow> */}
           <TableRow>
             <TableCell sx={{ fontSize: "17px", fontWeight: 550 }}>
-            <Typography sx={{fontSize:'20px', fontWeight:550}}>
-
-              Total MDM IDs
+              <Typography sx={{ fontSize: '20px', fontWeight: 550 }}>
+                Total MDM IDs
               </Typography>
             </TableCell>
             <TableCell>{excelData?.length}</TableCell>
             <TableCell></TableCell>
             <TableCell>
-                            <Button
+              <Button
                 key={'Add/ModifyAccountId'}
-                component={Link}  
-                to={"/"}   
+                component={Link}
+                to={"/"}
                 sx={{
                   my: 2,
                   display: 'block',
-                  textDecoration: 'underline', // Underline the text
+                  textDecoration: 'underline',
                   '&:hover': {
-                    textDecoration: 'underline', // Ensure underline on hover as well
+                    textDecoration: 'underline',
                   },
                 }}
               >
@@ -80,39 +69,25 @@ const VisualizationComponent = () => {
         </TableHead>
         <TableBody>
           <TableRow>
-            <TableCell><Typography sx={{fontSize:'20px'}}>Large run</Typography></TableCell>
+            <TableCell><Typography sx={{ fontSize: '20px' }}>Large run</Typography></TableCell>
             <TableCell>
-            <Typography sx={{fontSize:'20px'}}>
-              {
-                excelData?.filter((data) => data.AccountRefresh === "Large run")
-                  ?.length
-              }
+              <Typography sx={{ fontSize: '20px' }}>
+                {excelData?.filter((data) => data.AccountRefresh === "Large run")?.length}
               </Typography>
             </TableCell>
             <TableCell></TableCell>
             <TableCell>
-              {/* <Button
-                variant="contained"
-                sx={{
-                  backgroundColor: "grey",
-                }}
-
-                // onClick={()=>largeRunViewHandler()}
-              >
-                View
-              </Button> */}
-                            <Button
+              <Button
                 key={'largeRunView'}
-                // component={Link}  
-                // to={"/"}   
                 sx={{
                   my: 2,
                   display: 'block',
-                  textDecoration: 'underline', // Underline the text
+                  textDecoration: 'underline',
                   '&:hover': {
-                    textDecoration: 'underline', // Ensure underline on hover as well
+                    textDecoration: 'underline',
                   },
                 }}
+                onClick={largeRunViewHandler}
               >
                 View
               </Button>
@@ -120,32 +95,28 @@ const VisualizationComponent = () => {
           </TableRow>
           <TableRow>
             <TableCell>
-            <Typography sx={{fontSize:'20px'}}>
-              Quick run
+              <Typography sx={{ fontSize: '20px' }}>
+                Quick run
               </Typography>
-              </TableCell>
+            </TableCell>
             <TableCell>
-            <Typography sx={{fontSize:'20px'}}>
-              {
-                excelData?.filter((data) => data.AccountRefresh === "Quick run")
-                  ?.length
-              }
+              <Typography sx={{ fontSize: '20px' }}>
+                {excelData?.filter((data) => data.AccountRefresh === "Quick run")?.length}
               </Typography>
             </TableCell>
             <TableCell></TableCell>
             <TableCell>
-            <Button
+              <Button
                 key={'quickRunView'}
-                // component={Link}  
-                // to={"/"}   
                 sx={{
                   my: 2,
                   display: 'block',
-                  textDecoration: 'underline', 
+                  textDecoration: 'underline',
                   '&:hover': {
                     textDecoration: 'underline',
                   },
                 }}
+                onClick={quickRunViewHandler}
               >
                 View
               </Button>
