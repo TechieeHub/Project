061785bc0c475df6828eb500a8b155fa855d8954
TechieeHub <ishangupta141997@gmail.com>
2024-09-02 15:06:07 +0530

    Chnages

--- src/components/Charts/ChartData.js
+++ src/components/Charts/ChartData.js
@@ -1,14 +1,15 @@
-import { Box } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import VisualizationComponent from './VisualizationComponent';
-import AnomalieComponent from './AnomalieComponent';
-import ChartComponent from './ChartComponent';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { setTableData } from '../../Store/excelSlice';
+import { Box, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import VisualizationComponent from "./VisualizationComponent";
+import AnomalieComponent from "./AnomalieComponent";
+import ChartComponent from "./ChartComponent";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { setTableData } from "../../Store/excelSlice";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 const ChartData = () => {
-  const [filteredChartData, setFilteredChartData] = useState(null);
   const [initialChartData, setInitialChartData] = useState(null);
 
   const dispatch = useDispatch();
@@ -26,22 +27,82 @@ const ChartData = () => {
       .catch((error) => console.log("error", error));
   }, []);
 
-  const handleView = (data) => {
-    setFilteredChartData(data);
+  const handleExport = () => {
+    const input = document.getElementById("contentToExport");
+
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+
+      const pdf = new jsPDF();
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+
+      // Number of pages needed
+      let heightLeft = imgHeight;
+      let position = 0;
+
+      pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, pdfWidth * imgHeight / imgWidth);
+      heightLeft -= pdfHeight;
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, pdfWidth * imgHeight / imgWidth);
+        heightLeft -= pdfHeight;
+      }
+
+      pdf.save('exported-file.pdf');
+    });
   };
 
   return (
     <Box>
-      <Box sx={{ display: 'flex', gap: '10px', marginTop: '10px', marginLeft: '5px', marginRight: '5px' }}>
-        <VisualizationComponent onView={handleView} />
-        <AnomalieComponent onView={handleView} />
+      <Box 
+        sx={{ 
+          display: "flex", 
+          justifyContent: "flex-end",  
+          marginRight: '1%'
+        }}
+      >
+        <Button
+          variant="contained"
+          sx={{
+            marginTop: "30px",
+            backgroundColor: "grey",
+            width: "15%",
+          }}
+          onClick={() => handleExport()}
+        >
+          Export to Pdf
+        </Button>
       </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', marginTop: '30px' }}>
-        {filteredChartData?.length > 0 ? (
-          <ChartComponent data={filteredChartData} />
-        ) : (
-          initialChartData?.length > 0 && <ChartComponent data={initialChartData} />
-        )}
+      <Box id={"contentToExport"}>  {/* Content to be exported to PDF */}
+        <Box
+          sx={{
+            display: "flex",
+            gap: "10px",
+            marginTop: "10px",
+            marginLeft: "5px",
+            marginRight: "5px",
+          }}
+        >
+          <VisualizationComponent />
+          <AnomalieComponent />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "10px",
+            marginTop: "30px",
+          }}
+        >
+          {initialChartData?.length > 0 && <ChartComponent data={excelData} />}
+        </Box>
       </Box>
     </Box>
   );
