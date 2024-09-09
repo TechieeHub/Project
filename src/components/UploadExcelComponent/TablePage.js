ad00059bb7f1f4e46ddcf734e3e22ffbeb71a3bb
ShivangiInc <shivangimittal.0.4.0.1.9.6@gmail.com>
2024-09-09 14:25:56 +0530

    Added loader to this page

--- src/components/UploadExcelComponent/TablePage.js
+++ src/components/UploadExcelComponent/TablePage.js
@@ -1,30 +1,51 @@
-import { Box } from "@mui/material";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useDispatch } from "react-redux";
 import TableComponent from "../TableComponent/TableComponent";
+import { useDispatch } from "react-redux";
 import { setTableData } from "../../Store/excelSlice";
 import { useLocation } from "react-router-dom";
 
 const TablePage = () => {
   const [excelData, setExcelData] = useState(null);
   const [deletedColumns, setDeletedColumns] = useState(null);
+  const [loading, setLoading] = useState(true); // Added loading state
   const dispatch = useDispatch();
   const location = useLocation();
 
   useEffect(() => {
+    setLoading(true); // Set loading to true when the fetch starts
     axios
       .get("http://localhost:8000/api/data/")
       .then((response) => {
         setExcelData(response.data.records);
         setDeletedColumns(response.data.deleted_columns);
         dispatch(setTableData(response.data.records));
+        setLoading(false); // Set loading to false after the data is fetched
       })
-      .catch((error) => console.log("error", error));
+      .catch((error) => {
+        console.log("error", error);
+        setLoading(false); // Even on error, stop the loading spinner
+      });
   }, [dispatch]);
 
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return (
-    <Box>
+    <Box sx={{ margin: "1rem" }}>
       {excelData?.length > 0 ? (
         <TableComponent
           excelData={excelData}
@@ -33,7 +54,9 @@ const TablePage = () => {
           location={location}
         />
       ) : (
-        <Box sx={{ height: "40px", background: "#D3D3D3", marginTop: "3px" }}>
+        <Box
+          sx={{ height: "40px", background: "#D3D3D3", marginTop: "3px" }}
+        >
           No File is uploaded yet!
         </Box>
       )}
